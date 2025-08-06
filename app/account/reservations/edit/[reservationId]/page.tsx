1f6202363604d75c6382be81dd730c703eae302c
molamike <120581758+molamikedevs@users.noreply.github.com>
2025-08-06 16:02:07 +0400

    update reservation button

--- app/account/reservations/edit/[reservationId]/page.tsx
+++ app/account/reservations/edit/[reservationId]/page.tsx
@@ -10,54 +10,55 @@ const { numGuests, observations, suiteId } = reservation;
 
   const { maxCapacity } = await getSuiteById(suiteId);
   return (
-    <div>
-      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Edit Reservation #{reservationId}
-      </h2>
+		<div>
+			<h2 className="font-semibold text-2xl text-accent-400 mb-7">
+				Edit Reservation #{reservationId}
+			</h2>
 
-      <form action={updateReservationAction} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
-        <input
-          type="hidden"
-          name="reservationId"
-          value={reservationId}
-        />
-        <div className="space-y-2">
-          <label htmlFor="numGuests">How many guests?</label>
-          <select
-            name="numGuests"
-            defaultValue={numGuests}
-            id="numGuests"
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-            required
-          >
-            <option value="" key="">
-              Select number of guests...
-            </option>
-            {Array.from({ length: maxCapacity }, (_, i) => i + 1).map((x) => (
-              <option value={x} key={x}>
-                {x} {x === 1 ? "guest" : "guests"}
-              </option>
-            ))}
-          </select>
-        </div>
+			<form
+				action={updateReservationAction}
+				className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+				<input type="hidden" name="reservationId" value={reservationId} />
+				<div className="space-y-2">
+					<label htmlFor="numGuests">How many guests?</label>
+					<select
+						name="numGuests"
+						defaultValue={numGuests}
+						id="numGuests"
+						className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+						required>
+						<option value="" key="">
+							Select number of guests...
+						</option>
+						{Array.from({ length: maxCapacity }, (_, i) => i + 1).map(x => (
+							<option value={x} key={x}>
+								{x} {x === 1 ? 'guest' : 'guests'}
+							</option>
+						))}
+					</select>
+				</div>
 
-        <div className="space-y-2">
-          <label htmlFor="observations">
-            Anything we should know about your stay?
-          </label>
-          <textarea
-          defaultValue={observations}
-            name="observations"
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-          />
-        </div>
+				<div className="space-y-2">
+					<label htmlFor="observations">
+						Anything we should know about your stay?
+					</label>
+					<textarea
+						defaultValue={observations}
+						name="observations"
+						className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+					/>
+				</div>
 
-        <div className="flex justify-end items-center gap-6">
-          <div className="flex justify-end items-center gap-4 sm:gap-6 w-full">
-				<SubmitButton pendingLabel="Updating...">Update Reservation</SubmitButton>
-			</div>
-        </div>
-      </form>
-    </div>
-  );
+				<div className="flex justify-end items-center gap-6">
+					<div className="flex justify-end items-center gap-4 sm:gap-6 w-full">
+						<SubmitButton
+							pendingLabel="Updating..."
+							className="px-6 py-3 sm:px-8 sm:py-4">
+							Update Reservation
+						</SubmitButton>
+					</div>
+				</div>
+			</form>
+		</div>
+	)
 }
