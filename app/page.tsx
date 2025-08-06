b541e99675d44ec842f3023226365d76cf96ab2a
molamike <120581758+molamikedevs@users.noreply.github.com>
2025-08-06 16:13:08 +0400

    retouch the welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -17,15 +17,17 @@ export default function Page() {
 				}}
 			/>
 
-			<div className="relative z-10 text-center px-4">
-				<h1 className="text-4xl md:text-6xl lg:text-8xl text-primary-50 mb-10 md:mb-12 tracking-tight font-normal">
-					Welcome to paradise.
-				</h1>
-				<Link
-					href="/suites"
-					className="bg-primary-550 px-6 py-4 md:px-8 md:py-6 text-primary-50 text-base md:text-lg font-semibold hover:bg-primary-600 hover:scale-95 transition-all inline-block">
-					Explore luxury suites
-				</Link>
+			<div className="relative z-10 px-4 pt-[30vh] md:pt-0 md:block">
+				<div className="text-center">
+					<h1 className="text-4xl md:text-6xl lg:text-8xl text-primary-50 mb-14 md:mb-12 tracking-tight font-normal">
+						Welcome to paradise.
+					</h1>
+					<Link
+						href="/suites"
+						className="bg-primary-550 px-8 py-5 md:px-10 md:py-6 text-primary-50 text-lg md:text-xl font-semibold hover:bg-primary-600 hover:scale-95 transition-all inline-block">
+						Explore luxury suites
+					</Link>
+				</div>
 			</div>
 		</main>
 	)
